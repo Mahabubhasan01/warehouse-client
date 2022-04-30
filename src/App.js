11a284857969f0aef7a94da9a13c9b5a802568b5
Mahabubhasan01 <mahabubhasaan@outlook.com>
2022-04-30 10:06:16 +0600

    footer and navbar add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import useProductsHook from './Pages/CustomHooks/ProductsHook'
+import Home from './Pages/HomePage/Home';
+import CustomNavbar from './Pages/HomePage/Navbar/CustomNavbar';
 
 function App() {
   const [products,setProducts] = useProductsHook([])
   return (
-    <div className="App">
-      <h1>{products.length}</h1>
+    <div>
+      <CustomNavbar></CustomNavbar>
+
+      <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/inventory' element></Route>
+      </Routes>
     </div>
   );
 }
