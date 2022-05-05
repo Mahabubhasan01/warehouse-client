e2ad91971b302d82189f8c314078b73f654ce35c
Mahabubhasan01 <mahabubhasaan@outlook.com>
2022-05-05 20:44:38 +0600

    env file issue done

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -2,14 +2,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// Import the functions you need from the SDKs you need
-/* import { initializeApp } from "firebase/app";
- */// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_apiKey,
@@ -20,28 +12,6 @@ const firebaseConfig = {
     appId:process.env.REACT_APP_appId
 };
 
-// Initialize Firebase
-/* const app = initializeApp(firebaseConfig);
- */
-// "AIzaSyD6XMg2mh1st3gicB2i2Fu0kkiLIlgswHI"
-
-// update 
-// Import the functions you need from the SDKs you need
-/* import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyA94riFmqeslxE5yy_7N0rQZHhwl5XroGM",
-  authDomain: "eco-hub-eb09e.firebaseapp.com",
-  projectId: "eco-hub-eb09e",
-  storageBucket: "eco-hub-eb09e.appspot.com",
-  messagingSenderId: "846343240541",
-  appId: "1:846343240541:web:ef588f1a24f0d7fc3786b4"
-};
- */
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
