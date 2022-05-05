7fc14e2ca9912f81d49d8dcd9403b925e53c3f24
Mahabubhasan01 <mahabubhasaan@outlook.com>
2022-05-06 01:02:03 +0600

    first question js vs node js answer done

--- src/Pages/HomePage/Blogs/Blogs.js
+++ src/Pages/HomePage/Blogs/Blogs.js
@@ -4,27 +4,59 @@ const Blogs = () => {
   return (
     <div className="row container">
       <div class="card border-primary mb-3 col-12 col-sm-6 col-md-4 col-lg-4 my-3 mx-0 
-      " style={{maxWidth: "20rem"}}>
+      " >
         <div class="card-header"> Difference between javascript and nodejs</div>
-        <div class="card-body text-primary">
+        <div class="card-body text-dark">
           <h5 class="card-title">Primary card title</h5>
           <p class="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+              <h1 className="text-center text-info">Javascript</h1>
+            <ul>
+                <ol> Javascript is programming language</ol>
+                <li> Javascript is interpreted language</li>
+                <li> Javascript run any browser and also its new version is ES6</li>
+                <li> Javascript most using framework React,Angular,Vue,Next,Electron and so more</li>
+                <li> Javascript is on the working chrome v8 engin</li>
+                <li> Javascript is interpreted language</li>
+            </ul>
+              <h1 className="text-center text-info">Node Js</h1>
+            <ul>
+                <ol> Node js is run on the backend site or server side</ol>
+                <li> Node js has written C and C++</li>
+                <li> Node js its scalable and so smooth for web application</li>
+                <li> Node js most using framework Express and so more</li>
+                <li> Node js is only the working chrome v8 engin</li>
+                <li> Node js is runtime env for js and also use specific for js</li>
+            </ul>
           </p>
         </div>
       </div>
-      <div class="card border-secondary mb-3 col-12 col-sm-6 col-md-4 col-lg-4 my-3 mx-0 " style={{maxWidth: "20rem"}}>
+      <div class="card border-secondary mb-3 col-12 col-sm-6 col-md-4 col-lg-4 my-3 mx-0 " >
         <div class="card-header">When should you use nodejs and when should you use mongodb</div>
         <div class="card-body text-secondary">
           <h5 class="card-title">Secondary card title</h5>
           <p class="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+              <h1 className="text-center text-info">Javascript</h1>
+            <ul>
+                <ol> Javascript is programming language</ol>
+                <li> Javascript is interpreted language</li>
+                <li> Javascript run any browser and also its new version is ES6</li>
+                <li> Javascript most using framework React,Angular,Vue,Next,Electron and so more</li>
+                <li> Javascript is on the working chrome v8 engin</li>
+                <li> Javascript is interpreted language</li>
+            </ul>
+              <h1 className="text-center text-info">Node Js</h1>
+            <ul>
+                <ol> Node js is run on the backend site or server side</ol>
+                <li> Node js has written C and C++</li>
+                <li> Node js its scalable and so smooth for web application</li>
+                <li> Node js most using framework Express and so more</li>
+                <li> Node js is only the working chrome v8 engin</li>
+                <li> Node js is runtime env for js and also use specific for js</li>
+            </ul>
           </p>
         </div>
       </div>
-      <div class="card border-success mb-3 col-12 col-sm-6 col-md-4 col-lg-4 my-3 mx-0 " style={{maxWidth: "20rem"}}>
+      <div class="card border-success mb-3 col-12 col-sm-6 col-md-4 col-lg-4 my-3 mx-0 " >
         <div class="card-header">Differences between sql and nosql databases.</div>
         <div class="card-body text-success">
           <h5 class="card-title">Success card title</h5>
@@ -34,7 +66,7 @@ const Blogs = () => {
           </p>
         </div>
       </div>
-      <div class="card border-danger mb-3 col-12 col-sm-6 col-md-4 col-lg-4 my-3 mx-0 " style={{maxWidth: "20rem"}}>
+      <div class="card border-danger mb-3 col-12 col-sm-6 col-md-4 col-lg-4 my-3 mx-0 " >
         <div class="card-header">What is the purpose of jwt and how does it work</div>
         <div class="card-body text-danger">
           <h5 class="card-title">Danger card title</h5>
